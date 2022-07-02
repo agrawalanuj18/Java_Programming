a369899bac5327ee68763756d06cd992aef9ccb7
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-02 13:10:01 +0530

    start learning java

---
+++ Java/src/com/main/Main.java
@@ -0,0 +1,17 @@
+package com.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+        System.out.println("Hello world!");
+
+// Variable in Java
+        String firstName = "The";
+        String lastName = "Rock";
+        int age = 52;
+        String fullName = firstName + lastName;
+        System.out.println(firstName  + lastName + age);
+        System.out.println(fullName);
+
+    }
+}
