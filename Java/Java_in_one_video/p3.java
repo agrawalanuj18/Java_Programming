199be2772d9b44b09e1555f952ac94e016d75a12
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-16 23:42:14 +0530

    add some more pratice question basic

--- Java/Java_in_one_video/p3.java
+++ Java/Java_in_one_video/p3.java
@@ -1,5 +1,14 @@
 public class p3 {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        // Varables in java
+        String name = "Akku";
+        System.out.println(name);
+        int age = 18;
+        float salary = 155555.25f;
+
+        System.out.println(age + salary);
+        char firstLetter = 'a';
+        System.out.println(firstLetter);
     }
+
 }
---
+++ Java/Java_in_one_video/p4.java
@@ -0,0 +1,36 @@
+
+public class p4 {
+    public static void main(String[] args) {
+
+        byte age = 30;
+        int phone = 1234567890;
+        long phone2 = 1234567890123L;
+        float pi = 3.14F;
+        char letter = '@';
+        boolean isAdult = true;
+        System.out.println(age);
+        System.out.println(phone);
+        System.out.println(phone2);
+        System.out.println(pi);
+        System.out.println(letter);
+        System.out.println(isAdult);
+
+        // Data Types in Java
+        // Basically, two types Exit
+        // one is Primitve and Second one is Not Primitive
+
+        // *********Primitive data types are***********
+
+        // byte - 1 bit [-128 to 127]
+        // short - 2
+        // int - 4 1,2,34......
+        // long -8
+        // float -4 eg:-3.14
+        // double -8
+        // char - 2 Bit a,b,c,d,e..........
+        // boolean -1 True/False
+
+        // Each data type have some range, so always comsider it
+
+    }
+}
