de41c0ed547e0a3a5b0cd78cf29f09992cede0f2
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-17 00:13:10 +0530

    pratice String method question

---
+++ Java/Java_in_one_video/p5.java
@@ -0,0 +1,13 @@
+public class p5 {
+    public static void main(String[] args){
+        // Non-Primitive Data Types
+        // Basically we perform many other task in Non primitve data Types
+
+        String name = "Amannnn";
+        System.out.println(name.length());
+        
+        String friend = new String("Amannnnhhhhhhhhhh");
+        System.out.println(friend.length());
+        
+    }
+}
---
+++ Java/Java_in_one_video/p6.java
@@ -0,0 +1,22 @@
+public class p6 {
+    public static void main(String[] args){
+
+        // Concatenate String  
+
+        String name1 = "Akku";
+        String name2 = "Aman";
+        String name3 = name1 + " and " + name2;
+        System.out.println(name3);
+
+        //charAt on String 
+        // Position 0 se start hoti h 
+
+        System.out.println(name1.charAt(2));
+
+        // Length
+        
+
+
+
+    }
+}
