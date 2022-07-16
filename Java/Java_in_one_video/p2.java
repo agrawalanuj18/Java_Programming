11e6101ed78cc1b52c1c2c749eb156b26f6a1e5f
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-16 23:12:50 +0530

    Java in one Shot by Apna college

---
+++ Java/Java_in_one_video/p2.java
@@ -0,0 +1,11 @@
+public class p2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("How to write comments in Java");
+        //this is use for single line comment 
+        /*
+         * this is multi-line comment
+         */
+    }
+}
