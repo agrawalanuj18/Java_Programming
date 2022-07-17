c4a7086774abd49a6b787166ea23fbef5e9fae35
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-17 16:59:06 +0530

    Number Guessing Game

---
+++ Java/Java_in_one_video/NumberGuessingGame.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+
+        int ans = (int) (Math.random() * 100);
+
+        int guessNum;
+        do {
+
+            System.out.println("Enter Your Guess No.");
+            Scanner sc = new Scanner(System.in);
+            guessNum = sc.nextInt();
+
+            if (guessNum == ans) {
+                System.out.println("You win");
+                break;
+            } else if (guessNum < ans) {
+                System.out.println("Your Guess Number is Low");
+            } else {
+                System.out.println("Your Guess Number is High");
+            }
+        } while (guessNum > 0);
+        System.out.println("Done");
+    }
+}
--- Java/Java_in_one_video/p19.java
+++ Java/Java_in_one_video/p19.java
@@ -1,5 +1,17 @@
 public class p19 {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        // Do-While Loop
+        // 1-100
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 100);
+
+        do {
+            --i;
+            System.out.println(i);
+
+        } while (i >= 1);
     }
 }
