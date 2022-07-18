85848206ea13f4c4e20b089c7b986ae157334292
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-07-18 19:50:01 +0530

    polymorphism

--- OOPs/Oops in one shot/polymorphism.java
+++ OOPs/Oops in one shot/polymorphism.java
@@ -1,6 +1,28 @@
- 
+class Student {
+    String name;
+    int age;
 
+    public void printInfo(String name) {
+        System.out.println(name);
+    }
+
+    public void printInfo(int age) {
+        System.out.println(age);
+    }
+
+    public void printInfo(String name, int age) {
+        System.out.println(name + " " + age);
+    }
+}
 
 public class polymorphism {
-    
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Aman";
+        s1.age = 20;
+
+        s1.printInfo(s1.name);
+        s1.printInfo(s1.age);
+        s1.printInfo(s1.name, s1.age);
+    }
 }
