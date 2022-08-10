b3c4b0aa85ae792af29e1065b134e625b0466901
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-08-10 14:49:31 +0530

    solve the error in output

--- Java/4_PraticeSet/q9_0_1Triangle.java
+++ Java/4_PraticeSet/q9_0_1Triangle.java
@@ -1,12 +1,33 @@
 public class q9_0_1Triangle {
     public static void main(String[] args) {
-        int k = 1;
+
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(k%2 + " ");
-                k++;
+                int Sum = i + j;
+                if (Sum % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
             }
             System.out.println();
         }
+ // // This give an similar but wrong OUTPUT
+        // int k = 1;
+        // for (int i = 1; i <= 5; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(k%2 + " ");
+        // k++;
+        // }
+        // System.out.println();
+        // }
+       
+        // output :-
+        // 1
+        // 0 1
+        // 0 1 0
+        // 1 0 1 0
+        // 1 0 1 0 1
+        
     }
 }
