72ac753d5f0f9cf50af8c6a243d86135ecc194e7
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-05 21:50:49 +0530

    Selection Sort

--- DSA/Sorting/SelectionSort.java
+++ DSA/Sorting/SelectionSort.java
@@ -1,5 +1,26 @@
 public class SelectionSort {
     public static void main(String[] args) {
-        
+        System.out.println("Learning Bubble Sort");
+
+        int arr[] = { 7, 8, 3, 1, 2 };
+
+        // time complexity = O(n^2)
+        // Selection Sort
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
