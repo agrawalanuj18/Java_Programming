44301772744485b4eb1fdda429c2f1b2ffcfd7c3
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-07 01:46:05 +0530

    Java

---
+++ Java/01_Basics/KJava/First Java Program/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("You enter the Input: " + a);
+    }
+}
