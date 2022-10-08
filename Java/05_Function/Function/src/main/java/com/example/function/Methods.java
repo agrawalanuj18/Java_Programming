98907c60a9d51787d66a74e85ca74f1a32677a3a
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-08 23:30:10 +0530

    Solve

--- Java/05_Function/Function/src/main/java/com/example/function/Methods.java
+++ Java/05_Function/Function/src/main/java/com/example/function/Methods.java
@@ -1,7 +1,18 @@
 package com.example.function;
 
+import java.util.Scanner;
+
 public class Methods {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        Sum(a, b);
 
+    }
+
+    static void Sum(int a, int b) {
+       int sum = a + b;
+        System.out.println(sum);
     }
 }
