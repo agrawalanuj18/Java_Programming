372dca7fd92b8dbc148457c0cbe2054842ace79d
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-09 23:06:03 +0530

    Array

---
+++ Java/06_Array/Array/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Started Learning Array");
+        // Array is a collection of data types.
+        // Syntax
+        // datatype[] variacle_name = new datatypes  
+    }
+}
