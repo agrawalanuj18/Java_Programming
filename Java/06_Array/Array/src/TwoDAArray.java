384b9661f01b84a9ff5247cada2fa53933130cab
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-10 23:50:05 +0530

    java

---
+++ Java/06_Array/Array/src/TwoDAArray.java
@@ -0,0 +1,13 @@
+//import java.util.Arrays;
+
+public class TwoDAArray {
+    public static void main(String[] args) {
+//        int [][] arr = new int[3][];
+        int [][] arr = {
+                {1, 3, 4},
+                {1, 3, 5},
+                {43, 555, 67}
+                            };
+        System.out.println(arr[2][2]);
+    }
+}
