55f3d61d308596842909881b5ce5dbfdf7de61e6
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-10 17:25:03 +0530

    add

--- Java/06_Array/Array/src/Main.java
+++ Java/06_Array/Array/src/Main.java
@@ -3,6 +3,12 @@ public class Main {
         System.out.println("Started Learning Array");
         // Array is a collection of data types.
         // Syntax
-        // datatype[] variacle_name = new datatypes  
+        // datatype[] variacle_name = new datatypes
+        int[] arr = {233, 333, 222, 222, 222};
+        System.out.print("[" );
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i] + "," );
+        }
+        System.out.print("]");
     }
 }
