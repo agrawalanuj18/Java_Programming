55adcc33ec4a9af7edbae03a8667b648962923ca
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-11 23:38:47 +0530

    add

--- Java/06_Array/Array/src/TwoDAArray.java
+++ Java/06_Array/Array/src/TwoDAArray.java
@@ -4,9 +4,9 @@ public class TwoDAArray {
     public static void main(String[] args) {
 //        int [][] arr = new int[3][];
         int [][] arr = {
-                {1, 3, 4},
-                {1, 3, 5},
-                {43, 555, 67}
+                {1, 3, 4}, // 0th Index
+                {1, 3}, // 1st index
+                {43, 555, 67, 3,5} // 2nd Index
                             };
         System.out.println(arr[2][2]);
     }
