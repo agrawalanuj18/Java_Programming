27bdcdf1e2bafdb9af35ae9558f031cfff417892
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-14 19:16:43 +0530

    add

--- Java/06_Array/Question/src/main/java/com/example/question/Question2.java
+++ Java/06_Array/Question/src/main/java/com/example/question/Question2.java
@@ -3,5 +3,12 @@ package com.example.question;
 public class Question2 {
     public static void main(String[] args) {
         System.out.println("Practicing Java");
+        for (int i = 0; i < ; i++) {
+            
+        }
+        for (:
+             ) {
+            
+        }
     }
 }
