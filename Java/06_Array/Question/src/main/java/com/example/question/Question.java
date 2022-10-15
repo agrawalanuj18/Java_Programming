d136e3c57e818b16cf437f908bfc14c97af15315
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-10-15 21:11:53 +0530

    Add

--- Java/06_Array/Question/src/main/java/com/example/question/Question.java
+++
@@ -1,7 +0,0 @@
-package com.example.question;
-
-public class Question {
-    public static void main(String[] args) {
-        System.out.print("Hello World");
-    }
-}
--- Java/06_Array/Question/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.question {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.question to javafx.fxml;
-    exports com.example.question;
-}
