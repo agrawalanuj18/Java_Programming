1574d04ba16c061e5db320913ecd0776ce9cb3c8
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-11-05 23:42:36 +0530

    add

---
+++ Java/00_HackerRank/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        Scanner str = new Scanner(System.in);
+        String s = str.nextLine();  
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
