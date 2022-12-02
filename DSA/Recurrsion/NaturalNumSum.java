f067bcc2b9d575232161621f768b25a9db278295
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-12-02 23:41:06 +0530

    add

--- DSA/Recurrsion/NaturalNumSum.java
+++ DSA/Recurrsion/NaturalNumSum.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class NaturalNumSum {
     public static void PrintSum(int n, int Sum, int j) {
         Sum += j;
@@ -8,7 +10,10 @@ public class NaturalNumSum {
         PrintSum(n, Sum, j + 1);
     }
     public static void main(String[] args) {
-
-        PrintSum(10, 0, 1);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:- ");
+        int n = sc.nextInt();
+        sc.close();
+        PrintSum(n, 0, 1);
     }
 }
