c67453148f2063e092201c55c88e702aadaa9a69
agrawalanuj18 <agrawalanuj8622@gmail.com>
2022-12-03 23:49:48 +0530

    add

---
+++ DSA/Recurrsion/tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+public static void printNum(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        printNum(n-1);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        printNum(n);
+    }
