b0b2b7d1762673acfa9972075fca5952dac77b15
Anuj Agrawal <agrawalanuj8622@gmail.com>
2024-05-25 22:59:49 +0530

    pratice

--- DSA/Sorting/Bubble.java
+++ DSA/Sorting/Bubble.java
@@ -9,11 +9,9 @@ public class Bubble {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // swap the value
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-
                 }
             }
         }
--- DSA/Sorting/tempCodeRunnerFile.java
+++
@@ -1,3 +0,0 @@
-for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
